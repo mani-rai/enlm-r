577b9d170ca7eb0c6ffdfe208951deebb266cb10
mani <rai.mani.37@gmail.com>
2022-07-21 00:35:37 +0545

    Project init

---
+++ data.py
@@ -0,0 +1,167 @@
+import logging
+from random import choice
+
+import torch
+from datasets import load_dataset
+from tokenizers.implementations import ByteLevelBPETokenizer
+from torch.utils.data import IterableDataset
+
+
+class EnlmrLanguageSpecificDataset(IterableDataset):
+
+    def __init__(self, name, dataset, tokenizer, buffer_size=1000, max_token=512, is_valid=False):
+        self.name = name
+        self.setup_logging()
+        self.dataset = dataset
+        self.tokenizer = tokenizer
+        self.buffer_size = buffer_size
+        self.max_token = max_token
+        self.dataset_iterator = iter(self.dataset)
+        self.buffer = []
+        self.rem = None
+        self.is_valid = is_valid
+        self.initialize_buffer()
+
+    def setup_logging(self):
+        self.logger = logging.getLogger(self.name)
+        logging.basicConfig(format="%(asctime)s - %(levelname)s - %(name)s - %(message)s", datefmt="%m/%d/%Y %H:%M:%S",
+                            level=logging.INFO)
+
+    def initialize_buffer(self):
+        self.logger.info("Starting buffer initialization.")
+        while len(self.buffer) < self.buffer_size:
+            sample = self.get_sample()
+            if sample is None:
+                break
+            if len(sample) > self.max_token:
+                continue
+            self.buffer.append(sample)
+        self.logger.info("Buffer initialized.")
+
+    def get_sample(self):
+        seq = [self.tokenizer.convert_tokens_to_ids('<s>')]
+        bos = True
+        eod = False
+        while True:
+            if self.rem is not None:
+                seq = seq + self.rem
+                self.rem = None
+                bos = False
+            try:
+                line_sample = next(self.dataset_iterator)
+            except StopIteration:
+                seq.append(self.tokenizer.convert_tokens_to_ids('</s>'))
+                if len(seq) < 3:
+                    if self.is_valid:
+                        return
+                    else:
+                        seq = [self.tokenizer.convert_tokens_to_ids('<s>')]
+                        self.dataset_iterator = iter(self.dataset)
+                        continue
+                else:
+                    if self.is_valid:
+                        return seq
+                    else:
+                        self.dataset_iterator = iter(self.dataset)
+                        continue
+            sen = line_sample['text']
+            sen = sen.strip()
+            if len(sen) == 0:
+                if bos:
+                    continue
+                elif not eod:
+                    if len(seq) + 1 <= self.max_token:
+                        seq.append(self.tokenizer.convert_tokens_to_ids('</s>'))
+                    eod = True
+                continue
+            eod = False
+            bos = False
+            encodes = self.tokenizer.encode(sen, add_special_tokens=False)
+            encodes.append(self.tokenizer.convert_tokens_to_ids('</s>'))
+            if len(seq) + len(encodes) <= self.max_token:
+                seq = seq + encodes
+            else:
+                self.rem = encodes
+                break
+        return seq
+
+    def __iter__(self):
+        while True:
+            if len(self.buffer) == 0:
+                return
+            index = choice(list(range(len(self.buffer))))
+            sample = self.buffer[index]
+            new_sample = self.get_sample()
+            if new_sample is not None:
+                self.buffer[index] = new_sample
+            else:
+                self.buffer = self.buffer[:index] + self.buffer[index + 1:]
+            yield sample
+
+    def __next__(self):
+        if len(self.buffer) == 0:
+            raise StopIteration
+        index = choice(list(range(len(self.buffer))))
+        sample = self.buffer[index]
+        new_sample = self.get_sample()
+        if new_sample is not None:
+            self.buffer[index] = new_sample
+        else:
+            self.buffer = self.buffer[:index] + self.buffer[index + 1:]
+        return sample
+
+
+class EnlmrCombinedDataset(IterableDataset):
+
+    def __init__(self, datasets, batch_size=32, max_tokens=512):
+        self.datasets = datasets
+        self.batch_size = batch_size
+        self.max_tokens = max_tokens
+
+    def __iter__(self):
+        indexes = list(range(len(self.datasets)))
+        index = None
+        count = 1
+        while True:
+            if len(indexes) == 0:
+                return
+            if count == 1 or index not in indexes or index is None:
+                index = choice(indexes)
+            try:
+                sample = next(self.datasets[index])
+            except StopIteration:
+                indexes.remove(index)
+                continue
+            if count >= self.batch_size:
+                count = 1
+            else:
+                count += 1
+            sample_tensor = torch.tensor(sample)
+            yield {'input_ids': sample_tensor, 'attention_mask': torch.ones_like(sample_tensor)}
+
+
+class ConvertedDataset:
+
+    def __init__(self, dataset):
+        self.dataset = list(dataset)
+
+    def __getitem__(self, index):
+        return self.dataset[index]
+
+    def __len__(self):
+        return len(self.dataset)
+
+
+if __name__ == '__main__':
+    en_raw_ds = load_dataset('text', data_files="data/cc100-10k.en", split='train')
+    ne_raw_ds = load_dataset('text', data_files="data/cc100-10k.ne", split='train')
+
+    tokenizer = ByteLevelBPETokenizer('configs/enlm-r-vocab.json', 'configs/enlm-r-merges.txt')
+
+    en_ds = EnlmrLanguageSpecificDataset('en', en_raw_ds, tokenizer)
+    ne_ds = EnlmrLanguageSpecificDataset('ne', ne_raw_ds, tokenizer)
+
+    data = iter(EnlmrCombinedDataset([en_ds, ne_ds], tokenizer, 3))
+
+    for sample in data:
+        print(tokenizer.decode(sample.tolist()))
---
+++ train.py
@@ -0,0 +1,106 @@
+import os
+from datetime import datetime
+
+import transformers
+from datasets import load_dataset
+from transformers import XLMRobertaTokenizer, DataCollatorForLanguageModeling, XLMRobertaConfig, XLMRobertaForMaskedLM, \
+    TrainingArguments, SchedulerType, IntervalStrategy
+
+import wandb
+from data import EnlmrCombinedDataset, EnlmrLanguageSpecificDataset, ConvertedDataset
+
+
+class Trainer:
+
+    def __init__(self):
+        self.batch_size = 8
+        self.max_token = 512
+
+    def load_datasets(self, tokenizer):
+        en_ds = load_dataset('text', data_files="data/cc100-10k.en", split='train').train_test_split(test_size=0.1,
+                                                                                                     shuffle=False)
+        ne_ds = load_dataset('text', data_files="data/cc100-10k.ne", split='train').train_test_split(test_size=0.1,
+                                                                                                     shuffle=False)
+
+        en_train_ds = EnlmrLanguageSpecificDataset('English Train Dataset', en_ds['train'], tokenizer)
+        en_valid_ds = EnlmrLanguageSpecificDataset('English Valid Dataset', en_ds['test'], tokenizer, is_valid=True)
+        ne_train_ds = EnlmrLanguageSpecificDataset('Nepali Train Dataset', ne_ds['train'], tokenizer)
+        ne_valid_ds = EnlmrLanguageSpecificDataset('Nepali Valid Dataset', ne_ds['test'], tokenizer, is_valid=True)
+
+        train_ds = EnlmrCombinedDataset([en_train_ds, ne_train_ds], batch_size=self.batch_size)
+        valid_ds = ConvertedDataset(EnlmrCombinedDataset([en_valid_ds, ne_valid_ds], batch_size=self.batch_size))
+
+        return train_ds, valid_ds
+
+    def train(self):
+        tokenizer = XLMRobertaTokenizer('sentencepiece/enlm-r.spm.model',
+                                        sp_model_kwargs={'enable_sampling': True, 'nbest_size': 64, 'alpha': 0.1},
+                                        model_max_length=self.max_token, name_or_path='enlm-r-base')
+        train_ds, valid_ds = self.load_datasets(tokenizer)
+        data_collator = DataCollatorForLanguageModeling(tokenizer=tokenizer)
+        config = XLMRobertaConfig(vocab_size=tokenizer.vocab_size, max_position_embeddings=514, type_vocab_size=1,
+                                  layer_norm_eps=1e-05, output_past=True, num_hidden_layers=1, num_attention_heads=1)
+        model = XLMRobertaForMaskedLM(config=config)
+
+        run_name = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        training_args = TrainingArguments(
+            output_dir="checkpoints",
+            overwrite_output_dir=True,
+            warmup_steps=50,
+            max_steps=1000,
+            logging_steps=50,
+            save_steps=50,
+            # bf16=True,
+            fp16=True,
+            # tpu_num_cores=8,
+            # eval_steps=2,
+            # push_to_hub=True,
+            # include_inputs_for_metrics=True,
+            per_device_train_batch_size=self.batch_size,
+            per_device_eval_batch_size=self.batch_size,
+
+            adam_beta1=0.9,
+            adam_beta2=0.98,
+            adam_epsilon=1e-06,
+            evaluation_strategy='steps',
+            weight_decay=0.01,
+            learning_rate=0.0006,
+            lr_scheduler_type=SchedulerType.POLYNOMIAL,
+            log_level='info',
+            logging_dir='logs',
+            logging_strategy=IntervalStrategy.STEPS,
+            save_strategy='steps',
+            save_total_limit=20,
+            seed=1,
+            run_name=run_name,
+            load_best_model_at_end=True,
+            greater_is_better=False,
+            report_to=['wandb'],
+            hub_model_id="enlm-r",
+            hub_strategy='checkpoint',
+            hub_token='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',
+            hub_private_repo=True,
+        )
+
+        trainer = transformers.Trainer(
+            model=model,
+            args=training_args,
+            data_collator=data_collator,
+            train_dataset=train_ds,
+            eval_dataset=valid_ds,
+            tokenizer=tokenizer,
+        )
+
+        trainer.train()
+
+
+def main():
+    wandb.init(project="enlm-r")
+    trainer = Trainer()
+    trainer.train()
+
+
+os.environ["WANDB_DISABLED"] = "false"
+
+if __name__ == '__main__':
+    main()
